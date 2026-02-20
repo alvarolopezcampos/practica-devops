b03f3a38fd62c322ea3ae9a4d6db7bd69250f932
Álvaro López <240365255+alvarolopezcampos@users.noreply.github.com>
2026-02-20 11:41:51 +0100

    Delete src/main/java/com/example directory

--- src/main/java/com/example/App.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hola Jenkins! La aplicacion funciona correctamente.");
-    }
-}
